7281379b5cf8497be75da4aaad89db01fa025c6e
achesak <achesak@yahoo.com>
2013-10-06 18:08:27 -0500

    First commit.

---
+++ noaa.nim
@@ -0,0 +1,12 @@
+# NOAA weather API wrapper
+
+# Written by Adam Chesak.
+# Code released under the MIT open source license.
+
+# Import modules.
+import httpclient
+import cgi
+import strutils
+import xmlparser
+import xmltree
+import streams
